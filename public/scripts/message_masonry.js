76f8dfff7c03d01e030da8080d37665bf7db1397
Jaydon Soo <jaydonsky@gmail.com>
2024-11-26 08:58:23 +0800

    Improve behaviour of initial layout
    - Set a height on msg-container before applying Masonry to prevent loss
    of scroll position
    - Make the pre-masonry layout have the same column gutter as the masonry
    layout

--- public/scripts/message_masonry.js
+++ public/scripts/message_masonry.js
@@ -1,4 +1,7 @@
 const msg_container = document.getElementById("msg-container");
+// Set style height to current height so the container doesn't momentarily shrink to 0 when applying Masonry,
+// which causes scroll position to be lost
+msg_container.style.height = `${msg_container.clientHeight}px`;
 const message_masonry = new Masonry(msg_container, {
     itemSelector: ".msg",
     gutter: 20,
